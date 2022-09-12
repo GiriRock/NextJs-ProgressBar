2e1b05e7769f32475e4e848daaa9f7d74d18b0df
Giridharan <giridharanrock@gmail.com>
2022-09-12 11:42:57 +0530

    first commit

---
+++ pages/blog.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+function about() {
+  return (
+    <div><><h1>Next.Js Progress Bar Indicator</h1><ul>
+    <div className='flex'>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    
+    
+      <Link href="/about">
+        <a>About Us</a>
+      </Link>
+    
+      <Link href="/blog">
+        <a>Blog Post</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact Us</a>
+      </Link>
+    </div>
+    
+  <h3>Blog</h3>
+  </ul></></div>
+  )
+}
+export default about
